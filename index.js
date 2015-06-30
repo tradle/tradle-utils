4b0838b34ee93612e6a75c38faff1311701f8460
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-06-29 20:00:20 -0400

    add standard style

--- index.js
+++ index.js
@@ -1,164 +1,166 @@
-'use strict';
+'use strict'
 
-var assert = require('assert');
-var fs = require('fs');
-var path = require('path');
-var crypto = require('crypto');
-var createTorrent = require('create-torrent');
-var parseTorrent = require('parse-torrent');
-var defaults = require('defaults');
-var mkdirp = require('mkdirp');
-var stringify = require('json-stable-stringify');
+var assert = require('assert')
+var fs = require('fs')
+var path = require('path')
+var crypto = require('crypto')
+var createTorrent = require('create-torrent')
+var parseTorrent = require('parse-torrent')
+var defaults = require('defaults')
+var mkdirp = require('mkdirp')
+var stringify = require('json-stable-stringify')
 var bitcoin = require('bitcoinjs-lib')
-var CTR = 'aes-256-ctr';
+var CTR = 'aes-256-ctr'
 
 var utils = {
-  createTorrent: function(data, options, callback) {
-    if (typeof data === 'string') console.warn('Interpreting data as file path: ' + data);
+  createTorrent: function (data, options, callback) {
+    if (typeof data === 'string') console.warn('Interpreting data as file path: ' + data)
 
     if (typeof options === 'function') {
-      callback = options;
-      options = null;
+      callback = options
+      options = null
     }
 
-    var name = utils.getTorrentName(data);
+    var name = utils.getTorrentName(data)
     options = defaults(options || {}, {
       name: name
-    });
+    })
 
-    assert(options.name === name, 'Torrent name must be ' + name);
+    assert(options.name === name, 'Torrent name must be ' + name)
 
-    createTorrent(data, options, function(err, torrent) {
-      if (err) return callback(err);
+    createTorrent(data, options, function (err, torrent) {
+      if (err) return callback(err)
 
-      callback(null, parseTorrent(torrent));
-    });
-  },
-
-  getInfoHash: function(data, callback) {
-    utils.createTorrent(data, function(err, torrent) {
-      if (err) return callback(err);
-
-      callback(null, torrent.infoHash);
-    });
-  },
-
-  getStorageKeyFor: function(data, callback) {
-    utils.getInfoHash(data, function(err, infoHash) {
-      callback(err, new Buffer(infoHash, 'hex'));
+      callback(null, parseTorrent(torrent))
     })
   },
 
-  getTorrentName: function(val) {
-    return crypto.createHash('sha256').update(val).digest('hex');
+  getInfoHash: function (data, callback) {
+    utils.createTorrent(data, function (err, torrent) {
+      if (err) return callback(err)
+
+      callback(null, torrent.infoHash)
+    })
   },
 
-  httpError: function(code, msg) {
-    var err = new Error(msg);
-    err.code = code;
-    return err;
+  getStorageKeyFor: function (data, callback) {
+    utils.getInfoHash(data, function (err, infoHash) {
+      callback(err, new Buffer(infoHash, 'hex'))
+    })
   },
 
-  isTruthy: function(val) {
-    if (typeof val === 'undefined' || val === null || val === false) return false;
-    if (val instanceof Number) return !!val;
-
-    return val !== '0' && val !== 'false';
+  getTorrentName: function (val) {
+    return crypto.createHash('sha256').update(val).digest('hex')
   },
 
-  requireOption: function(options, option) {
-    if (!(option in options)) throw new Error('Missing required option: ' + option);
-
-    return options[option];
+  httpError: function (code, msg) {
+    var err = new Error(msg)
+    err.code = code
+    return err
   },
 
-  requireOptions: function(options /*[, option1, option2, ...]*/) {
-    [].slice.call(arguments, 1).map(function(arg) {
-      utils.requireOption(options, arg);
-    });
+  isTruthy: function (val) {
+    if (typeof val === 'undefined' || val === null || val === false) return false
+    if (val instanceof Number) return !!val
+
+    return val !== '0' && val !== 'false'
   },
 
-  writeFile: function(options, callback) {
-    var filePath = utils.requireOption(options, 'path');
-    filePath = path.resolve(filePath);
-    var data = utils.requireOption(options, 'data');
+  requireOption: function (options, option) {
+    if (!(option in options)) throw new Error('Missing required option: ' + option)
+
+    return options[option]
+  },
+
+  requireOptions: function (options /*[, option1, option2, ...]*/) {
+    [].slice.call(arguments, 1).map(function (arg) {
+      utils.requireOption(options, arg)
+    })
+  },
+
+  writeFile: function (options, callback) {
+    var filePath = utils.requireOption(options, 'path')
+    filePath = path.resolve(filePath)
+    var data = utils.requireOption(options, 'data')
     var fileOptions = options.options || {
-      encoding: 'utf8'
-    };
+        encoding: 'utf8'
+      }
     var tmpPath = options.safe ?
       filePath + '.' + crypto.randomBytes(8).toString('hex') + '.tmp' :
-      filePath;
+      filePath
 
-    mkdirp(path.dirname(filePath), function(err) {
-      if (err) return callback(err);
+    mkdirp(path.dirname(filePath), function (err) {
+      if (err) return callback(err)
 
-      fs.writeFile(tmpPath, data, fileOptions, function(err) {
-        if (err) return callback(err);
+      fs.writeFile(tmpPath, data, fileOptions, function (err) {
+        if (err) return callback(err)
 
-        if (!options.safe) return callback();
+        if (!options.safe) return callback()
 
-        fs.rename(tmpPath, filePath, function(err) {
-          if (err) return callback(err);
+        fs.rename(tmpPath, filePath, function (err) {
+          if (err) return callback(err)
 
-          callback();
+          callback()
         })
-      });
-    });
+      })
+    })
   },
 
-  proxyFunctions: function(proxy, source) {
+  proxyFunctions: function (proxy, source) {
     for (var p in source) {
-      if (!proxy[p] && typeof source[p] === 'function')
-        proxy[p] = source[p].bind(source);
+      if (!proxy[p] && typeof source[p] === 'function') {
+        proxy[p] = source[p].bind(source)
+      }
     }
   },
 
-  bindPrototypeFunctions: function(obj) {
+  bindPrototypeFunctions: function (obj) {
     // bind all prototype functions to self
-    var proto = obj.constructor.prototype;
+    var proto = obj.constructor.prototype
     for (var p in proto) {
-      var val = proto[p];
-      if (typeof val === 'function')
-        obj[p] = obj[p].bind(obj);
+      var val = proto[p]
+      if (typeof val === 'function') {
+        obj[p] = obj[p].bind(obj)
+      }
     }
   },
 
-  requireParam: function(paramName, paramValue) {
-    if (typeof paramValue === 'undefined') throw new Error('Missing required parameter: ' + paramName);
+  requireParam: function (paramName, paramValue) {
+    if (typeof paramValue === 'undefined') throw new Error('Missing required parameter: ' + paramName)
 
-    return paramValue;
+    return paramValue
   },
 
-  prettify: function(obj) {
-    if (typeof obj === 'string') return obj;
+  prettify: function (obj) {
+    if (typeof obj === 'string') return obj
 
-    return stringify(obj, { space: 2 });
+    return stringify(obj, { space: 2 })
   },
 
-  stringify: function(obj) {
-    if (typeof obj === 'string') return obj;
+  stringify: function (obj) {
+    if (typeof obj === 'string') return obj
 
-    return stringify(obj);
+    return stringify(obj)
   },
 
-  getAddressFromInput: function(input, networkName) {
+  getAddressFromInput: function (input, networkName) {
     if (bitcoin.scripts.classifyInput(input.script) === 'pubkeyhash') {
-      var network = bitcoin.networks[networkName];
+      var network = bitcoin.networks[networkName]
       return bitcoin.ECPubKey.fromBuffer(input.script.chunks[1])
         .getAddress(network)
-        .toString();
+        .toString()
     }
   },
 
-  getAddressFromOutput: function(output, networkName) {
+  getAddressFromOutput: function (output, networkName) {
     if (bitcoin.scripts.classifyOutput(output.script) === 'pubkeyhash') {
       return bitcoin.Address
         .fromOutputScript(output.script, bitcoin.networks[networkName])
-        .toString();
+        .toString()
     }
   },
 
-  getOpReturnData: function(tx) {
+  getOpReturnData: function (tx) {
     if (typeof tx === 'string') tx = bitcoin.Transaction.fromHex(tx)
     else if (Buffer.isBuffer(tx)) tx = bitcoin.Transaction.fromBuffer(tx)
 
@@ -170,47 +172,47 @@ var utils = {
     }
   },
 
-  sharedSecret: function(aPriv, bPub) {
+  sharedSecret: function (aPriv, bPub) {
     if (typeof aPriv === 'string') aPriv = bitcoin.ECKey.fromWIF(aPriv)
     if (typeof bPub === 'string') bPub = bitcoin.ECPubKey.fromHex(bPub)
 
-    aPriv = aPriv.d || aPriv;
+    aPriv = aPriv.d || aPriv
     var shared = bPub.Q.multiply(aPriv).getEncoded(true)
     // cut off version byte 0x02/0x03
     // https://github.com/cryptocoinjs/ecurve/blob/master/lib/point.js#L207
-    if (shared.length === 33) shared = shared.slice(1);
+    if (shared.length === 33) shared = shared.slice(1)
 
-    return shared;
+    return shared
   },
 
-  sharedEncryptionKey: function(aPriv, bPub) {
-    var sharedSecret = utils.sharedSecret(aPriv, bPub);
-    return crypto.createHash('sha256').update(sharedSecret).digest();
+  sharedEncryptionKey: function (aPriv, bPub) {
+    var sharedSecret = utils.sharedSecret(aPriv, bPub)
+    return crypto.createHash('sha256').update(sharedSecret).digest()
   },
 
-  encrypt: function(text, password) {
-    assert(text && password, 'text and password are both required');
+  encrypt: function (text, password) {
+    assert(text && password, 'text and password are both required')
 
-    var cipher = crypto.createCipher(CTR, password);
-    return updateCipher(cipher, text);
+    var cipher = crypto.createCipher(CTR, password)
+    return updateCipher(cipher, text)
   },
 
-  decrypt: function(text, password) {
-    assert(text && password, 'text and password are both required');
+  decrypt: function (text, password) {
+    assert(text && password, 'text and password are both required')
 
-    var decipher = crypto.createDecipher(CTR, password);
-    return updateDecipher(decipher, text);
+    var decipher = crypto.createDecipher(CTR, password)
+    return updateDecipher(decipher, text)
   }
 }
 
-function updateCipher(cipher, data) {
-  if (Buffer.isBuffer(data)) return Buffer.concat([cipher.update(data), cipher.final()]);
-  else return cipher.update(data, 'utf8', 'base64') + cipher.final('base64');
+function updateCipher (cipher, data) {
+  if (Buffer.isBuffer(data)) return Buffer.concat([cipher.update(data), cipher.final()])
+  else return cipher.update(data, 'utf8', 'base64') + cipher.final('base64')
 }
 
-function updateDecipher(decipher, data) {
-  if (Buffer.isBuffer(data)) return Buffer.concat([decipher.update(data), decipher.final()]);
-  else return decipher.update(data, 'base64', 'utf8') + decipher.final('utf8');
+function updateDecipher (decipher, data) {
+  if (Buffer.isBuffer(data)) return Buffer.concat([decipher.update(data), decipher.final()])
+  else return decipher.update(data, 'base64', 'utf8') + decipher.final('utf8')
 }
 
-module.exports = utils;
+module.exports = utils
--- test/index.js
+++ test/index.js
@@ -1,36 +1,35 @@
-
 var crypto = require('crypto')
 var test = require('tape')
 var bufferEqual = require('buffer-equal')
 var ECKey = require('bitcoinjs-lib').ECKey
 var utils = require('../')
 
-test('aes encrypt/decrypt', function(t) {
-  t.plan(2);
+test('aes encrypt/decrypt', function (t) {
+  t.plan(2)
 
-  var buf = crypto.randomBytes(32);
-  var key = crypto.randomBytes(32);
-  var encrypted = utils.encrypt(buf, key);
-  var decrypted = utils.decrypt(encrypted, key);
+  var buf = crypto.randomBytes(32)
+  var key = crypto.randomBytes(32)
+  var encrypted = utils.encrypt(buf, key)
+  var decrypted = utils.decrypt(encrypted, key)
 
-  t.ok(!bufferEqual(buf, encrypted));
-  t.ok(bufferEqual(buf, decrypted));
+  t.ok(!bufferEqual(buf, encrypted))
+  t.ok(bufferEqual(buf, decrypted))
 
-  buf = crypto.randomBytes(128);
-  // t.ok(bufferEqual(buf, utils.fileToBuf(utils.fileToString(buf))));
-});
+  buf = crypto.randomBytes(128)
+// t.ok(bufferEqual(buf, utils.fileToBuf(utils.fileToString(buf))))
+})
 
-test('ecdh', function(t) {
-  t.plan(1);
+test('ecdh', function (t) {
+  t.plan(1)
 
-  var a = ECKey.makeRandom();
-  var b = ECKey.makeRandom();
+  var a = ECKey.makeRandom()
+  var b = ECKey.makeRandom()
 
-  // var ab = a.pub.Q.multiply(b.d).getEncoded();
-  // var ba = b.pub.Q.multiply(a.d).getEncoded();
+  // var ab = a.pub.Q.multiply(b.d).getEncoded()
+  // var ba = b.pub.Q.multiply(a.d).getEncoded()
 
-  var ab = utils.sharedEncryptionKey(a.d, b.pub);
-  var ba = utils.sharedEncryptionKey(b.d, a.pub);
+  var ab = utils.sharedEncryptionKey(a.d, b.pub)
+  var ba = utils.sharedEncryptionKey(b.d, a.pub)
 
-  t.ok(bufferEqual(ab, ba));
-});
+  t.ok(bufferEqual(ab, ba))
+})
